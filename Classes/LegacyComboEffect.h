e3a7ebd9bf900bbf2c55c4de41c066e58b606294
babygogogo <101599838@qq.com>
2015-05-07 21:21:59 +0800

    Remove most legacy classes.

--- Classes/LegacyComboEffect.h
+++
@@ -1,6 +0,0 @@
-#ifndef _COMBO_EFFECT_H_
-#define _COMBO_EFFECT_H_
-#include "cocos2d.h"
-
-void showComboEffect(int size,cocos2d::Node* node);
-#endif
--- Classes/StarParticle.h
+++
@@ -1,7 +0,0 @@
-#ifndef _STAR_PARTICLE_H_
-#define _STAR_PARTICLE_H_
-#include "cocos2d.h"
-
-void showStarParticleEffect(int color,cocos2d::Point position,cocos2d::Node* node);
-cocos2d::Color4F getColor4F(int color);
-#endif
