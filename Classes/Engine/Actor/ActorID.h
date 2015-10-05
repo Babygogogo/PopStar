6746960465f7e7f8d2172d45b62c62ffe6ead533
Babygogogo <101599838@qq.com>
2015-10-05 17:57:08 +0800

    Revert "Change the engine to cocos2dx-3.8.1."
    This reverts commit 32acc997a1124f133994d74845f7222cf7344ae4.

--- Classes/Engine/Actor/ActorID.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ACTOR_ID__
-#define __ACTOR_ID__
-
-using ActorID = unsigned long;
-
-const ActorID INVALID_ACTOR_ID = 0;
-
-#endif // !__ACTOR_ID__
--- Classes/Engine/Actor/SoundPlayer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SOUND_PLAYER__
-#define __SOUND_PLAYER__
-
-class SoundPlayer
-{
-
-};
-
-#endif //__SOUND_PLAYER__
